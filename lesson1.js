4af477d5b8521a449fc994c89872b98d26353a0e
ivancard <ivcardenas@alumno.unlam.edu.ar>
2022-08-09 17:50:41 -0300

    lesson1 updated

--- lesson1.js
+++ lesson1.js
@@ -2,14 +2,11 @@ const {crearArchivo} = require('./helper/multiplicar')
 console.clear()
 
 
-process.argv.forEach(arg => {
-  console.log(`${arg} \n`)
-});
-// const base = 3;
+const base = 3;
 
-// crearArchivo(base)
-//   .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-//   .catch(err => console.log(err));
-//
+crearArchivo(base)
+  .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+  .catch(err => console.log(err));
+
 
 
