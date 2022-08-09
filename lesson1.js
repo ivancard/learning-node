9443b778abe532168d2bd43f692c71ac1bc91523
ivancard <ivcardenas@alumno.unlam.edu.ar>
2022-08-09 16:35:42 -0300

    first commit

---
+++ lesson1.js
@@ -0,0 +1,15 @@
+const {crearArchivo} = require('./helper/multiplicar')
+console.clear()
+
+
+process.argv.forEach(arg => {
+  console.log(`${arg} \n`)
+});
+// const base = 3;
+
+// crearArchivo(base)
+//   .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+//   .catch(err => console.log(err));
+//
+
+
